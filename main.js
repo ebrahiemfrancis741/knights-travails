761742cf973ebb204c22246539eb154eb8ee538f
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-02-15 05:09:48 +0200

    Add knightMoves(), solve() and reconstruct() functions

--- main.js
+++ main.js
@@ -16,17 +16,14 @@ function createAdjacencyList() {
   ];
 
   let adjacencyList = [];
-  // yes... tripple loop is terrible...
   for (let i = 0; i <= 7; i++) {
     for (let j = 0; j <= 7; j++) {
       let key = `${j},${i}`;
       adjacencyList[key] = [];
       for (let k = 0; k < moves.length; k++) {
-        // test to see if a move would be valid from this position
         let x = j + moves[k][0];
         let y = i + moves[k][1];
         if (x < 0 || x > 7 || y < 0 || y > 7) {
-          // invalid index results
           continue;
         }
         adjacencyList[key].push([x, y]);
@@ -47,5 +44,73 @@ function getValue(array) {
   return graph[key];
 }
 
+/*
+  Converts a 'x,y' key to [x,y] array
+*/
+function convertToArray(key) {
+  let values = key.split(",");
+  let x = parseInt(values[0]);
+  let y = parseInt(values[1]);
+  return [x, y];
+}
+
+function convertToKey(array){
+  let [x, y] = array;
+  let key = `${x},${y}`;
+  return key;
+}
+
 let graph = createAdjacencyList();
-console.log(graph);
+
+function solve(start) {
+  let queue = [start];
+  let visited = {};
+  let prev = {};
+
+  for (let key in graph) {
+    visited[key] = false;
+    prev[key] = null;
+  }
+
+  visited[start] = true;
+
+  while (queue.length > 0) {
+    let node = queue.shift();
+    let neighbors = graph[node];
+
+    for (let neighbor of neighbors) {
+      let neighborKey = convertToKey(neighbor);
+      if (!visited[neighborKey]) {
+        queue.push(neighborKey);
+        visited[neighborKey] = true;
+        prev[neighborKey] = node;
+      }
+    }
+  }
+
+  return prev;
+}
+
+function reconstructPath(start, end, prev) {
+  let path = [];
+  for (let at = end; at != null; at = prev[at]) {
+    path.push(convertToArray(at));
+  }
+  path.reverse();
+
+  if (path[0].toString() === convertToArray(start).toString()) {
+    return path;
+  }
+  return [];
+}
+
+function knightMoves(start, end) {
+  start = convertToKey(start);
+  end = convertToKey(end);
+
+  let prev = solve(start);
+
+  return reconstructPath(start, end, prev);
+}
+
+console.log(knightMoves([3, 3], [4, 3])); // Should find the shortest path from [3, 3] to [4, 3]
